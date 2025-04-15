dbfa7667256c780f83dc7626e7601125bcf726bd
Qlinyang223 <15163423867@163.com>
2025-04-15 19:27:29 +0800

    Add files via upload

---
+++ 三色球问题.py
@@ -0,0 +1,5 @@
+for huang in range(0, 4):
+    for hong in range(0, 4):
+        for lv in range(0, 4):
+            if hong + huang + lv == 8:
+                print(f"红球{hong}个， 黄球{huang}个，绿球{lv}个")
